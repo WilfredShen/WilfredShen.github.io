06c310e129a931669bb327e4463698931d93aa4d
WilfredShen <54000440+WilfredShen@users.noreply.github.com>
2019-08-24 15:09:16 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
-    $('.content-wrapper').attr('display', 'block')
+    $('.content-wrapper').attr('style', 'display: block')
   })
 
   if (window.location.hash && window.location.hash == '#blog') {
