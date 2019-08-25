6b5b2405567c5edb8a4cd0fcb36e1b3987bee1b7
WilfredShen <54000440+WilfredShen@users.noreply.github.com>
2019-08-25 17:32:50 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -45,12 +45,12 @@ $(document).ready(function () {
 
   $('.btn-mobile-menu__icon').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    $('.btn-mobile-menu__icon').toggleClass('fa-close fa-bars animated fadeIn')
   })
 
   $('.navigation-wrapper .blog-button').click(function () {
     $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    $('.btn-mobile-menu__icon').toggleClass('fa-close fa-bars animated fadeIn')
   })
 
   $('.backToTop').click(smoothscroll)
